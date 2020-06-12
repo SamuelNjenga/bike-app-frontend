4bb4b4941df3022802d7ed387320f6517ef68be9
Samuel Njenga <snjenga704@gmail.com>
2020-06-12 22:40:56 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -17,35 +17,37 @@ import Details from './MyAccountComponent/Details';
 import MyAccount from './MyAccountComponent/MyAccount';
 import Order from './MyAccountComponent/Order';
 import SellerRegister from './SellerComponent/SellerRegister';
-import BikeRegistration from './BikeRegistration/BikeRegistration'
+import BikeRegistration from './BikeRegistration/BikeRegistration';
 import { UserProvider } from './UserComponent/UserContext';
-
+import { CompanyProvider } from './CompanyContext/CompanyContext';
 function App() {
 	return (
 		<CartProvider>
 			<LoginProvider>
 				<UserProvider>
-					<div>
-						<Router>
-							<Switch>
-								<Route path="/cart" exact={true} component={Cart} />
-								<Route path="/myaccount" exact={true} component={MyAccount} />
-								<Route path="/bikedescription" exact={true} component={BikeDescription} />
-								<Route path="/" exact={true} component={Home} />
-								<Route path="/sellerregister" exact={true} component={SellerRegister} />
-								<Route path="/myorder" exact={true} component={Order} />
-								<Route path="/changepassword" exact={true} component={ChangePassword} />
-								<Route path="/personaldetails" exact={true} component={Details} />
-								<Route path="/bike" exact={true} component={BikeList} />
-								<Route path="/navbar" exact={true} component={Navbar} />
-								<Route path="/companyRegistration" exact={true} component={CompanyRegistration} />
-								<Route path="/login" exact={true} component={Login} />
-								<Route path="/logout" exact={true} component={Logout} />
-								<Route path="/contact" exact={true} component={Contact} />
-								<Route path="/bikeRegistration" exact={true} component={BikeRegistration} />
-							</Switch>
-						</Router>
-					</div>
+					<CompanyProvider>
+						<div>
+							<Router>
+								<Switch>
+									<Route path="/cart" exact={true} component={Cart} />
+									<Route path="/myaccount" exact={true} component={MyAccount} />
+									<Route path="/bikedescription" exact={true} component={BikeDescription} />
+									<Route path="/" exact={true} component={Home} />
+									<Route path="/sellerregister" exact={true} component={SellerRegister} />
+									<Route path="/myorder" exact={true} component={Order} />
+									<Route path="/changepassword" exact={true} component={ChangePassword} />
+									<Route path="/personaldetails" exact={true} component={Details} />
+									<Route path="/bike" exact={true} component={BikeList} />
+									<Route path="/navbar" exact={true} component={Navbar} />
+									<Route path="/companyRegistration" exact={true} component={CompanyRegistration} />
+									<Route path="/login" exact={true} component={Login} />
+									<Route path="/logout" exact={true} component={Logout} />
+									<Route path="/contact" exact={true} component={Contact} />
+									<Route path="/bikeRegistration" exact={true} component={BikeRegistration} />
+								</Switch>
+							</Router>
+						</div>
+					</CompanyProvider>
 				</UserProvider>
 			</LoginProvider>
 		</CartProvider>
