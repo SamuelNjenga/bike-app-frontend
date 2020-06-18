2b8df4d69078bcc016e73af65ec9a0d93deef4ce
Samuel Njenga <snjenga704@gmail.com>
2020-06-18 08:19:21 +0300

    Added Burger.js

--- src/NavbarComponent/Navbar.js
+++ src/NavbarComponent/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Burger from './Burger';
 const Nav = styled.nav`
 	width: 100%;
 	height: 65px;
@@ -17,7 +18,7 @@ const Navbar = () => {
 	return (
 		<Nav>
 			<div className="logo">Nav Bar</div>
-			
+			<Burger />
 		</Nav>
 	);
 };
