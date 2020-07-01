e5fe62d148453514a8fdc5dd8228b0af20838e92
Samuel Njenga <snjenga704@gmail.com>
2020-07-01 21:18:34 +0300

    Included StripeCheckout component to my Checkout.js

--- src/CartComponents/Checkout.js
+++ src/CartComponents/Checkout.js
@@ -3,10 +3,12 @@ import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { CartContext } from './CartContext';
 import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout'
 
 const Checkout = () => {
 	const { cart, setCart } = useContext(CartContext);
 	const [ cartss ] = cart;
+	const totalPrice = cartss.reduce((acc, curr) => acc + curr.price * curr.count, 0);
 
 	const j = () => {
 		for (let i in cartss) {
@@ -20,6 +22,9 @@ const Checkout = () => {
 			}
 		}
 	};
+	function handleToken(token,addresses){
+     console.log({token,addresses})
+	}
 	return (
 		<div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
 			<Link to="/bike" style={{ textDecoration: 'none' }}>
@@ -27,9 +32,16 @@ const Checkout = () => {
 					CONTINUE SHOPPING
 				</Button>
 			</Link>
-			<Button variant="contained" style={{ backgroundColor: 'lightblue', marginRight: '15px' }} onClick={j}>
+			{/* <Button variant="contained" style={{ backgroundColor: 'lightblue', marginRight: '15px' }} onClick={j}>
 				PROCEED TO CHECKOUT
-			</Button>
+			</Button> */}
+			<StripeCheckout 
+				stripeKey="pk_test_51Gzi4aF9Tjnqa5oo4vSqEViMF7RC2g3PjhYCKt7dYA8gt2gF9Qw4eLiKjcApyphzooIiu6Cceh5VT253xzn2gJbX00lbX3Brqv"
+				token={handleToken}
+				billingAddress
+				shippingAddress
+				amount={totalPrice * 100}
+			/>
 		</div>
 	);
 };
