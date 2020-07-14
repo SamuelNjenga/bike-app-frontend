bb221e793bf96d69b90701f8d515ba1db8613991
Samuel Njenga <snjenga704@gmail.com>
2020-07-14 21:53:42 +0300

    Modified Order.js in MyAccountComponent directory by adding a get endpoint to fetch orders table from the database for a specific user

--- src/MyAccountComponent/Order.js
+++ src/MyAccountComponent/Order.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../NavbarComponent/Navbar';
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
 const Order = () => {
+	const [ orders, setOrders ] = useState([]);
+	const [ loading, setLoading ] = useState(true);
+	const customerIdNumber = JSON.parse(localStorage.getItem('userId'));
+	const set = () =>
+		axios
+			.get(`http://localhost:3001/api/orders/${customerIdNumber}`)
+			.then((res) => {
+				setOrders(res.data);
+				setLoading(false);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	useEffect(() => {
+		set();
+	}, []);
 	return (
 		<div>
 			<Navbar />
-			<Typography>My Orders So Far</Typography>
-			
+			<Typography>My Bike Orders So Far</Typography>
+			{loading ? <CircularProgress /> : orders.map((item) => 
+			<>
+			<li key={item.bikeId}>{item.bikeId.brandName} {item.numberOfItemsOrdered}</li>
+			</>
+			)}
 		</div>
 	);
 };
